0e8410f206d01a1b462f6da61a03d3b602cbbf49
Sanjay <visvasrk@localhost.localdomain>
2025-09-08 15:28:14 +0530

    updated

--- backups/2025-09-08/app.py
+++ backups/2025-09-08/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, Response
 from datetime import datetime
+import time
 
 app = Flask(__name__)
 
@@ -15,6 +16,7 @@ def home():
 @app.route('/subjects/biology')
 def biology():
 	topics = {"biotechnology" : "This topic is about unit nine of grade 12 biology book. Both Biotechnology, Principles and Processes and its Applications are included.",
+	"organisms_and_populations" : "This topic is about how different organisms interact with each other in a population, the factors affecting it and study of ecology.",
 
 	}
 	topic_count = len(topics)
@@ -28,8 +30,11 @@ def biology_topic(topic):
 def biotechnology():
 	return render_template('biology/biotechnology.html')
 
+@app.route('/subjects/biology/organisms_and_populations')
+def organisms_and_populations():
+	return render_template('biology/organisms_and_populations.html')
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True)
+	app.run(host="0.0.0.0", port=5000, debug=True,threaded=True)
 
 
