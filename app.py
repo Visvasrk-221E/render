a96e27e8cf9562fab30dbe3fd300b7a2b091cb25
Sanjay <visvasrk@localhost.localdomain>
2025-09-16 02:49:47 +0530

    added tools category

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, url_for, redirect, Response
+from flask import Flask, render_template, url_for, redirect, Response, send_from_directory
 from datetime import datetime
 import time
 import os
 
 port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
+docsdir = os.path.join(app.static_folder, "documents")
+
+
+@app.route('/dochub/<path:filename>')
+def download_file(filename):
+	return send_from_directory('static/documents', filename, as_attachment=True)
 
 @app.route('/')
 def redirect_to_home():
@@ -12,12 +18,17 @@ def redirect_to_home():
 
 @app.route('/home')
 def home():
+	docnum = len([f for f in os.listdir(docsdir)])
 	subjects = {"biology" : "Topics based on general biology as well as grade 12 biology can be learnt from here.[Grade 12]",
 		"cybersecurity" : "Topics based on cybersecurity, from school to college level or even masters can be learnt from here.[Basics]",
 	}
 	subject_count = len(subjects)
 	crt_time = datetime.now()
-	return render_template('index.html', time=crt_time, subjects=subjects, subject_count=subject_count)
+	return render_template('index.html', time=crt_time, subjects=subjects, subject_count=subject_count, docnum=docnum)
+
+@app.route('/dochub')
+def dochub():
+	return render_template('documents/index.html')
 
 @app.route('/subjects/<subject>')
 def route_subject(subject):
@@ -25,11 +36,12 @@ def route_subject(subject):
 
 @app.route('/subjects/biology')
 def biology():
+	docnum = len([f for f in os.listdir(docsdir) if "[bio]" in f])
 	topics = {
 	"organisms_and_populations" : "This topic is about how different organisms interact with each other in a population, the factors affecting it and study of ecology.[Ecology]",
 	}
 	topic_count = len(topics)
-	return render_template('biology/index.html', topics=topics, topic_count=topic_count)
+	return render_template('biology/index.html', topics=topics, topic_count=topic_count, docnum=docnum)
 
 @app.route('/subjects/cybersecurity')
 def cybersecurity():
@@ -72,6 +84,21 @@ def render_basic_tools(tool_name):
 def nmap():
 	return render_template('cybersecurity/basic_tools/nmap.html')
 
+@app.route('/dochub/biodoc')
+def biodoc():
+	docs = [f for f in os.listdir(docsdir) if "[bio]" in f]
+	print(docs)
+	return render_template('documents/biodoc.html',docs=docs)
+
+@app.route('/dochub/compdoc')
+def compdoc():
+	docs = [f for f in os.listdir(docsdir) if "[comp]" in f]
+	return render_template('documents/compdoc.html', docs=docs)
+
+@app.route('/dochub/weapdoc')
+def weapdoc():
+	docs = [f for f in os.listdir(docsdir) if "[weap]" in f]
+	return render_template('documents/weapdoc.html', docs=docs)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=port)
